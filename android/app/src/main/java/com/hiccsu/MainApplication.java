5d87f6744fc36adb8e53c47e08c8237273bd42a6
DESKTOP-JSO6F83\xiaohaoxiong <269812428@qq.com>
2017-05-04 20:47:08 +0800

    android 初始化

--- android/app/src/main/java/com/hiccsu/MainApplication.java
+++ android/app/src/main/java/com/hiccsu/MainApplication.java
@@ -10,37 +10,27 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new BlurViewPackage(),
-            new VectorIconsPackage(),
-            new SplashScreenReactPackage()
-      );
-    }
-  };
-
+public class MainApplication extends NavigationApplication  {
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    // Add additional packages you require here
+    return Arrays.<ReactPackage>asList(
+
+    );
+    // No need to add RnnPackage and MainReactPackage
+    // Simply return null if you do not have additional packages:
+    // return null;
   }
+
 }
